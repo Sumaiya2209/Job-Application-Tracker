561eb64fc3900678c193c8fc4f208c399ff35025
Sumaiya2209 <sumaiyajannt2209@gmail.com>
2026-02-24 14:07:28 +0600

    Complete Job application tracker

--- machine.js
+++ machine.js
@@ -17,9 +17,9 @@ function showOnly(id) {
     const interviewCount = document.getElementById('interview').innerText;
     const rejectedCount = document.getElementById('rejected').innerText;
     if (id === 'interviewed_jobs') {
-        jobNumb.innerText = interviewCount + ' of';
+        jobNumb.innerText = interviewCount + ' of ';
     } else if (id === 'rejected_jobs') {
-        jobNumb.innerText = rejectedCount + ' of';;
+        jobNumb.innerText = rejectedCount + ' of ';
     }
     else {
         jobNumb.innerText = '';
@@ -135,3 +135,37 @@ function updateRejectedPage(jobCardId) {
     rejectedJobs.appendChild(clonedCard);
 }
 
+
+
+/* remove job card */
+function removeJob(jobCardId , jobStatus) {
+    const jobCard = document.getElementById(jobCardId);
+    const totalJobsElement = document.getElementById('total_jobs');
+    let totalJobs = Number(totalJobsElement.innerText);
+    totalJobs--;
+    totalJobsElement.innerText = totalJobs;
+    document.getElementById('total').innerText = totalJobs;
+    if (jobCard) {
+        jobCard.remove();
+    
+    const status = document.getElementById(jobStatus).innerText;
+    if(status === 'Interview')
+    {
+        let count = document.getElementById('interview').innerText;
+        if(Number(count) > 0)
+        {
+            count--;
+            document.getElementById('interview').innerText = count;
+        }
+    }
+    else  if(status === 'Rejected')
+    {
+        let count = document.getElementById('rejected').innerText;
+        if(Number(count) > 0)
+        {
+            count--;
+            document.getElementById('rejected').innerText = count;
+        }
+    }
+}
+}
